4b9d8390b1e75698bb6020fb89a203e515ffac4e
lidianchao <lidianchao_520@163.com>
2017-06-20 10:07:25 +0800

    QQ聊天布局

---
+++ 15-QQ聊天布局/15-QQ聊天布局/FLViewController.h
@@ -0,0 +1,15 @@
+//
+//  FLViewController.h
+//  15-QQ聊天布局
+//
+//  Created by Liu Feng on 13-12-3.
+//  Copyright (c) 2013年 Liu Feng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FLViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UITableView *tableView;
+
+@property (weak, nonatomic) IBOutlet UITextField *messageField;
+@end
